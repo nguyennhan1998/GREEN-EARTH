aed321340374498086f64f6f3043e7f545641590
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-07-04 18:02:31 +0700

    vanh push article

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\Category;
+use App\Event;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function listArticle()
+    {
+        $articles =Article::paginate(20);
+        return view("admin.article.list", [
+            "articles" => $articles
+        ]);
+
+    }
+
+    public function newArticle()
+    {
+        $categories = Category::all();
+        $events = Event::all();
+        return view("admin.article.new",
+            [
+                "categories"=>$categories,
+                "events"=>$events
+            ]);
+    }
+
+    public function saveArticle(Request $request){
+        $request->validate([
+            "title"=>"required",
+            "description"=>"required",
+            "content"=>"required",
+            "category_id"=>"required",
+            "event_id"=>"required",
+
+        ]);
+        try {
+            Event::create([
+                "title"=>$request->get("title"),
+                "description"=>$request->get("description"),
+                "content"=>$request->get("content"),
+                "category_id"=>$request->get("category_id"),
+                "event_id"=>$request->get("event_id"),
+
+            ]);
+
+        }catch (\Exception $exception){
+//            return $exception->getMessage();
+        }
+        return redirect()->to("/admin/list-article");
+    }
+
+    public function editArticle($id){
+        $article = Article::findOrFail($id);
+        $categories =Category::all();
+        $event = Event::all();
+        return view("admin.article.edit",[
+            "article" => $article,
+            "categories"=>$categories,
+            "event"=>$event
+        ]);
+    }
+
+    public function updateArticle($id,Request $request){
+        $article = Article::findOrFail($id);
+        $request->validate([
+            "title"=>"required",
+            "description"=>"required",
+            "content"=>"required",
+            "category_id"=>"required",
+            "event_id"=>"required",
+        ]);
+        try {
+            $article->update([
+                "title"=>$request->get("title"),
+                "description"=>$request->get("description"),
+                "content"=>$request->get("content"),
+                "category_id"=>$request->get("category_id"),
+                "event_id"=>$request->get("event_id"),
+            ]);
+        }catch (\Exception $exception){
+            return redirect()->back();
+        }
+        return redirect()->to("/admin/list-article");
+    }
+
+    public function deleteArticle($id){
+        $article = Article::findOrFail($id);
+        try {
+            $article->delete();
+        }catch (\Exception $exception){
+
+        }
+        return redirect()->to("admin/list-article");
+    }
+}
--- resources/views/admin/article/edit.blade.php
+++ resources/views/admin/article/edit.blade.php
@@ -1,1 +1,67 @@
+@extends("admin.layout")
+@section("content")
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-plain">
+                        <div class="card-header card-header-primary">
+                            <h4 class="card-title mt-0">EDIT ARTICLE </h4>
+                            <p class="card-category"> ------------------------------------------------- </p>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <form role="form" action="{{url("admin/update-article/{$article->__get("id")}")}}" method="post">
+                                        @method("PUT")
+                                        @csrf
+                                        <div class="form-group">
+                                            <label>Title</label>
+                                            <input  value="{{$article->__get("title")}}" type="text" name="title" class="form-control" placeholder="...">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Description</label>
+                                            <input  value="{{$article->__get("description")}}" type="text" name="description" class="form-control" placeholder="...">
+                                        </div>
+                                        <div class="form-group">
+                                            <label id="content">Content</label>
+                                            <textarea class="form-control" id="summary-ckeditor" name="content"></textarea>
+                                            @include('ckfinder::setup')
+                                            <script>
+                                                CKFinder.setupCKEditor();
+                                                CKEDITOR.replace( 'summary-ckeditor', {
+                                                    filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
+                                                    filebrowserUploadMethod: 'form'
+                                                });
+                                            </script>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Category Id</label>
+                                            <select name="category_id" class="form-control">
+                                                @foreach($categories as $category)
+                                                    <option value="{{$category->__get("id")}}">{{$category->__get("title")}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Event Id</label>
+                                            <select name="event_id" class="form-control">
+                                                @foreach($events as $event)
+                                                    <option value="{{$event->__get("id")}}">{{$event->__get("title")}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-round">Submit</button>
+                                    </form>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
 
