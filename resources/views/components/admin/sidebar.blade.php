e7fb24cc76970a1eaff98c0c52de9693167c8303
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-07-13 21:32:29 +0700

    user-event-eventdetail

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -46,7 +46,7 @@ Admin GreenEarth        </a></div>
                 </a>
             </li>
             <li class="nav-item  ">
-                <a class="nav-link" href="{{"events"}}">
+                <a class="nav-link" href="{{"events/list"}}">
                     <i class="material-icons">content_paste</i>
                     <p>List Event</p>
                 </a>
--- routes/user.php
+++ routes/user.php
@@ -6,7 +6,7 @@ Route::get("/home","WebController@Home");
 Route::get("/blog","WebController@Blog");
 Route::get("/about","WebController@AboutUS");
 Route::get("/event","WebController@Event");
-Route::get("/event-detail","WebController@EventDetail");
+Route::get("/event-detail/{id}","WebController@EventDetail");
 Route::get("/projects","WebController@Projects");
 Route::get("/projects-detail","WebController@ProjectsDetail");
 Route::get("/contact","WebController@Contact");
