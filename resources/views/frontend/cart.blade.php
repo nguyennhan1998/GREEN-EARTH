eae3e9cd5735514bba2bc0e980b78ca3688bb8f1
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-07-19 22:49:11 +0700

    fix css

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -1,31 +1,12 @@
 @extends("frontend.layout")
 @section("content")
-<<<<<<< HEAD
-=======
-    <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="breadcrumb__text">
-                        <h2>Shopping Cart</h2>
-                        <div class="breadcrumb__option">
-                            <a href="./index.html">Home</a>
-                            <span>Shopping Cart</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Breadcrumb Section End -->
->>>>>>> 195e1dbe3152a8c4df5eca4e16d46cc547ca394b
+
 
     <!-- Shoping Cart Section Begin -->
     <section class="shoping-cart spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-<<<<<<< HEAD
                     <h2 style="text-align: center ; margin-top: 20px">Shopping Cart</h2>
                 </div>
                 <div class="col-lg-12" style="margin-bottom: 30px;margin-top: 30px;}">
@@ -34,13 +15,6 @@
                             <thead>
                             <tr >
                                 <th style="text-align: left">Products</th>
-=======
-                    <div class="shoping__cart__table">
-                        <table>
-                            <thead>
-                            <tr>
-                                <th class="shoping__product">Products</th>
->>>>>>> 195e1dbe3152a8c4df5eca4e16d46cc547ca394b
                                 <th>Price</th>
                                 <th>Quantity</th>
                                 <th>Total</th>
@@ -48,74 +22,6 @@
                             </tr>
                             </thead>
                             <tbody>
-<<<<<<< HEAD
-                            <tr>
-                                <td class="shoping__cart__item">
-                                    <img src="{{asset("img/cart/cart-1.jpg")}}" alt="">
-                                    <h5>Vegetable’s Package</h5>
-                                </td>
-                                <td class="shoping__cart__price">
-                                    $55.00
-                                </td>
-                                <td class="shoping__cart__quantity">
-                                    <div class="quantity">
-                                        <div class="pro-qty">
-                                            <input type="text" value="1">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="shoping__cart__total">
-                                    $110.00
-                                </td>
-                                <td class="shoping__cart__item__close">
-                                    <span class="icon_close"></span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="shoping__cart__item">
-                                    <img src="{{asset("img/cart/cart-2.jpg")}}" alt="">
-                                    <h5>Fresh Garden Vegetable</h5>
-                                </td>
-                                <td class="shoping__cart__price">
-                                    $39.00
-                                </td>
-                                <td class="shoping__cart__quantity">
-                                    <div class="quantity">
-                                        <div class="pro-qty">
-                                            <input type="text" value="1">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="shoping__cart__total">
-                                    $39.99
-                                </td>
-                                <td class="shoping__cart__item__close">
-                                    <span class="icon_close"></span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="shoping__cart__item">
-                                    <img src="img/cart/cart-3.jpg" alt="">
-                                    <h5>Organic Bananas</h5>
-                                </td>
-                                <td class="shoping__cart__price">
-                                    $69.00
-                                </td>
-                                <td class="shoping__cart__quantity">
-                                    <div class="quantity">
-                                        <div class="pro-qty">
-                                            <input type="text" value="1">
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="shoping__cart__total">
-                                    $69.99
-                                </td>
-                                <td class="shoping__cart__item__close">
-                                    <span class="icon_close"></span>
-                                </td>
-                            </tr>
-=======
                             @foreach($products as $p)
                                 <tr>
                                     <td class="shoping__cart__item">
@@ -140,7 +46,6 @@
                                     </td>
                                 </tr>
                             @endforeach
->>>>>>> 195e1dbe3152a8c4df5eca4e16d46cc547ca394b
                             </tbody>
                         </table>
                     </div>
@@ -169,25 +74,15 @@
                     <div class="shoping__checkout">
                         <h5>Cart Total</h5>
                         <ul>
-<<<<<<< HEAD
-                            <li>Subtotal <span>$454.98</span></li>
-                            <li>Total <span>$454.98</span></li>
-                        </ul>
                         <a href="#" class="primary-btn">PROCEED TO CHECKOUT</a>
-=======
                             <li>Subtotal <span>${{$grandTotal}}</span></li>
                             <li>Total <span>${{$grandTotal}}</span></li>
                         </ul>
                         <a href="{{url("/checkout")}}" class="primary-btn">PROCEED TO CHECKOUT</a>
->>>>>>> 195e1dbe3152a8c4df5eca4e16d46cc547ca394b
                     </div>
                 </div>
             </div>
         </div>
     </section>
-<<<<<<< HEAD
-    <!-- Shoping Cart Section End -->
-=======
->>>>>>> 195e1dbe3152a8c4df5eca4e16d46cc547ca394b
 
 @endsection
