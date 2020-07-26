9e70fd0dd4c2cff4c07123db640498e115c67d3b
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2020-07-26 09:32:49 +0700

    event

---
+++ database/migrations/2020_07_26_022626_create_table_donate.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableDonate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donate', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("image")->nullable();
+            $table->string("description");
+            $table->string("content");
+            $table->dateTime("start_at");
+            $table->dateTime("end_at");
+            $table->integer("target_money");
+            $table->unsignedBigInteger("view_count")->default(0);
+            $table->unsignedBigInteger("organization_id");
+            $table->timestamps();
+            $table->foreign("organization_id")->references("id")->on("organizations");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donate');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -250,6 +250,7 @@ class DatabaseSeeder extends Seeder
 
                 'user_id'=>1]
                 ]
+
      );
     }
 }
